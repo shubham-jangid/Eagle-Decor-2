3b8e36f7493cb4c94388c0cb50ef1c2ae46b7699
shubham-jangid/werfgth <shubhamjangid70@gmail.com>
2019-04-06 12:24:58 +0530

    rote dne

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
-const nodemailer = require("nodemailer");
 const path = require("path");
 
 const app = express();
 
+//importing the route
 const project = require("./routes/project");
 const blog = require("./routes/blog");
+const contact = require("./routes/contact");
 
-console.log(__dirname);
 //view engine setup
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
 //static folder
 app.use("/public", express.static(path.join(__dirname, "public")));
-// app.use("/", express.static(path.join(__dirname)));
 
 //body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// using the route middleware
 app.use("/project", project);
 app.use("/blog", blog);
+app.use("/contact", contact);
 
 app.get("/", function(req, res) {
   res.render("index");
@@ -48,58 +49,7 @@ app.get("/contact", function(req, res) {
 });
 
 // conatact feature
-app.post("/contact", (req, res) => {
-  console.log(req.body.name);
-  const output = `
-    <p> You have a new contact request </p>
-    <h3> Contact Details </h3>
-    <ul>
-    <li> Name:${req.body.name}</li>
-    <li> Email Address:${req.body.email}</li>
-    <li> Phone:${req.body.phone}</li>
-    </ul>
-    <h3> Message </h3>
-    <p> ${req.body.message}</p>
-    `;
-  nodemailer.createTestAccount((err, account) => {
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-      host: "mail.smtp2go.com",
-      port: 2525,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user: "eagledecor", // generated ethereal user
-        pass: "b3F6M3IyN3lvZTAw" // generated ethereal password
-      }
-    });
 
-    // setup email data with unicode symbols
-    let mailOptions = {
-      from: '"EagleDecor contact request  " <contact@eagledecor.in>', // sender address
-      to: "eagledecor1@gmail.com", // list of receivers
-      subject: "Hello ✔", // Subject line
-      text: "Hello world?", // plain text body
-      html: output // html body
-    };
-
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        return console.log(error);
-      }
-      console.log("Message sent: %s", info.messageId);
-      // Preview only available when sending through an Ethereal account
-      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-      res.render("contact", {
-        msg: "Thanks for contacting. We will get back to you soon "
-      });
-
-      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-      // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-    });
-  });
-});
-
-app.listen(3000, () => {
+app.listen(8080, () => {
   console.log("8080");
 });
--- routes/blog.js
+++ routes/blog.js
@@ -6,4 +6,15 @@ router.get("/", (req, res) => {
   res.render("blog");
 });
 
+router.get("/top-2019-interior-design-trends", (req, res) => {
+  res.render("blog/top-2019-interior-design-trends");
+});
+
+router.get("/6-popular-interior-design-styles", (req, res) => {
+  res.render("blog/6-popular-interior-design-styles");
+});
+
+router.get("/5-bedroom-style-tips", (req, res) => {
+  res.render("blog/5-bedroom-style-tips");
+});
 module.exports = router;
