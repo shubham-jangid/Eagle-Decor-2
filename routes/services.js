5a8d3ac93870c6a439e01723091597e38dc1a80f
shubham-jangid/werfgth <shubhamjangid70@gmail.com>
2019-04-14 12:58:14 +0530

    adreess and services pages added

--- routes/services.js
+++ routes/services.js
@@ -10,25 +10,29 @@ router.get("/architectural-services", function(req, res) {
   res.render("services/architectural-services");
 });
 
-router.get("/commercial-interior-designing", function(req, res) {
-  res.render("services/commercial-interior-designing");
-});
 router.get("/residential-interior-designing", function(req, res) {
   res.render("services/residential-interior-designing");
 });
-router.get("/architectural-services", function(req, res) {
-  res.render("services/architectural-services");
+router.get("/hospitality-interior-designing", function(req, res) {
+  res.render("services/hospitality-interior-designing");
 });
-router.get("/architectural-services", function(req, res) {
-  res.render("services/architectural-services");
+router.get("/commercial-interior-designing", function(req, res) {
+  res.render("services/commercial-interior-designing");
 });
-router.get("/architectural-services", function(req, res) {
-  res.render("services/architectural-services");
+router.get("/turn-Key-projects", function(req, res) {
+  res.render("services/turn-Key-projects");
 });
-router.get("/architectural-services", function(req, res) {
-  res.render("services/architectural-services");
+router.get("/interior-decoration", function(req, res) {
+  res.render("services/interior-decoration");
 });
-router.get("/architectural-services", function(req, res) {
-  res.render("services/architectural-services");
+router.get("/modular-kitchen", function(req, res) {
+  res.render("services/modular-kitchen");
 });
+router.get("/handmade-furniture", function(req, res) {
+  res.render("services/handmade-furniture");
+});
+router.get("/modular-furniture", function(req, res) {
+  res.render("services/modular-furniture");
+});
+
 module.exports = router;
