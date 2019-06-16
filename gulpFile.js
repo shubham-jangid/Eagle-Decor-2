24a6409f20f428f974b910bde565ba3d87c21f11
shubham-jangid/werfgth <shubhamjangid70@gmail.com>
2019-06-17 00:32:03 +0530

    script minified

--- gulpFile.js
+++ gulpFile.js
@@ -27,4 +27,31 @@ gulp.task(
       .pipe(gulp.dest("./public/css"));
   })
 );
-gulp.task("default", gulp.series(["css"]));
+
+gulp.task(
+  "script",
+  gulp.series(function() {
+    gulp
+      .src([
+        "public/js/jquery.min.js",
+        "public/js/jquery-migrate-3.0.1.min.js",
+        "public/js/popper.min.js",
+        "public/js/bootstrap.min.js ",
+        "public/js/jquery.easing.1.3.js ",
+        "public/js/jquery.waypoints.min.js",
+        "public/js/jquery.stellar.min.js",
+        "public/js/owl.carousel.min.js ",
+        "public/js/jquery.magnific-popup.min.js ",
+        "public/js/aos.js ",
+        "public/js/jquery.animateNumber.min.js",
+        "public/js/bootstrap-datepicker.js",
+        // "public/js/jquery.timepicker.min.js",
+        "public/js/scrollax.min.js",
+        "public/js/main.js"
+      ])
+      .pipe(concat("app.js"))
+      .pipe(uglify())
+      .pipe(gulp.dest("./public/js"));
+  })
+);
+gulp.task("default", gulp.series(["script", "css"]));
